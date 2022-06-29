1016a9f3dd72cb4e8ffffbbc54cfb8318ee71eff
Marc Hershey <marchershey@users.noreply.github.com>
2022-06-29 10:27:12 -0400

    changed host dashboard
    - changed primary colors

--- resources/views/layouts/host.blade.php
+++ resources/views/layouts/host.blade.php
@@ -1,50 +1,12 @@
 <x-layouts.base>
-    <!--
-  This example requires Tailwind CSS v2.0+
-  
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  const colors = require('tailwindcss/colors')
-  
-  module.exports = {
-    // ...
-    theme: {
-      extend: {
-        colors: {
-          rose: colors.rose,
-        },
-      },
-    },
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
-    <!--
-  This example requires updating your template:
-
-  ```
-  <html class="h-full bg-gray-100">
-  <body class="h-full">
-  ```
--->
     <div class="min-h-full">
-        <!--
-      When the mobile menu is open, add `overflow-hidden` to the `body` element to prevent double scrollbars
-  
-      Menu open: "fixed inset-0 z-40 overflow-y-auto", Menu closed: ""
-    -->
         <header class="bg-white shadow-sm lg:static lg:overflow-y-visible">
             <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div class="relative flex justify-between xl:grid xl:grid-cols-12 lg:gap-8">
                     <div class="flex md:absolute md:left-0 md:inset-y-0 lg:static xl:col-span-2">
                         <div class="flex items-center flex-shrink-0">
                             <a href="#">
-                                <img class="block w-auto h-8" src="https://tailwindui.com/img/logos/workflow-mark.svg?color=rose&shade=500" alt="Workflow">
+                                <x-logo :showText="false" iconSize="w-6 h-6" />
                             </a>
                         </div>
                     </div>
@@ -59,32 +21,18 @@
                                             <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
                                         </svg>
                                     </div>
-                                    <input id="search" name="search" class="block w-full py-2 pl-10 pr-3 text-sm placeholder-gray-500 bg-white border border-gray-300 rounded-md focus:outline-none focus:text-gray-900 focus:placeholder-gray-400 focus:ring-1 focus:ring-rose-500 focus:border-rose-500 sm:text-sm" placeholder="Search" type="search">
+                                    <input id="search" name="search" class="block w-full py-2 pl-10 pr-3 text-sm placeholder-gray-500 bg-white border border-gray-300 rounded-md focus:outline-none focus:text-gray-900 focus:placeholder-gray-400 focus:ring-1 focus:ring-primary focus:border-primary sm:text-sm" placeholder="Search" type="search">
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="flex items-center md:absolute md:right-0 md:inset-y-0 lg:hidden">
                         <!-- Mobile menu button -->
-                        <button type="button" class="inline-flex items-center justify-center p-2 -mx-2 text-gray-400 rounded-md hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-rose-500" aria-expanded="false">
+                        <button type="button" class="inline-flex items-center justify-center p-2 -mx-2 text-gray-400 rounded-md hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary" aria-expanded="false">
                             <span class="sr-only">Open menu</span>
-                            <!--
-                Icon when menu is closed.
-  
-                Heroicon name: outline/menu
-  
-                Menu open: "hidden", Menu closed: "block"
-              -->
                             <svg class="block w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
                             </svg>
-                            <!--
-                Icon when menu is open.
-  
-                Heroicon name: outline/x
-  
-                Menu open: "block", Menu closed: "hidden"
-              -->
                             <svg class="hidden w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                             </svg>
@@ -92,7 +40,7 @@
                     </div>
                     <div class="hidden lg:flex lg:items-center lg:justify-end xl:col-span-4">
                         <a href="#" class="text-sm font-medium text-gray-900 hover:underline"> Go Premium </a>
-                        <a href="#" class="flex-shrink-0 p-1 ml-5 text-gray-400 bg-white rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500">
+                        <a href="#" class="flex-shrink-0 p-1 ml-5 text-gray-400 bg-white rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary">
                             <span class="sr-only">View notifications</span>
                             <!-- Heroicon name: outline/bell -->
                             <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
@@ -103,7 +51,7 @@
                         <!-- Profile dropdown -->
                         <div class="relative flex-shrink-0 ml-5">
                             <div>
-                                <button type="button" class="flex bg-white rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                                <button type="button" class="flex bg-white rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                                     <span class="sr-only">Open user menu</span>
                                     <img class="w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
                                 </button>
@@ -129,7 +77,7 @@
                             </div>
                         </div>
 
-                        <a href="#" class="inline-flex items-center px-4 py-2 ml-6 text-sm font-medium text-white border border-transparent rounded-md shadow-sm bg-rose-600 hover:bg-rose-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500"> New Post </a>
+                        <a href="#" class="inline-flex items-center px-4 py-2 ml-6 text-sm font-medium text-white border border-transparent rounded-md shadow-sm bg-primary hover:bg-primary-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary"> New Post </a>
                     </div>
                 </div>
             </div>
@@ -155,7 +103,7 @@
                             <div class="text-base font-medium text-gray-800">Chelsea Hagon</div>
                             <div class="text-sm font-medium text-gray-500">chelsea.hagon@example.com</div>
                         </div>
-                        <button type="button" class="flex-shrink-0 p-1 ml-auto text-gray-400 bg-white rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500">
+                        <button type="button" class="flex-shrink-0 p-1 ml-auto text-gray-400 bg-white rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary">
                             <span class="sr-only">View notifications</span>
                             <!-- Heroicon name: outline/bell -->
                             <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
@@ -173,7 +121,7 @@
                 </div>
 
                 <div class="max-w-3xl px-4 mx-auto mt-6 sm:px-6">
-                    <a href="#" class="flex items-center justify-center w-full px-4 py-2 text-base font-medium text-white border border-transparent rounded-md shadow-sm bg-rose-600 hover:bg-rose-700"> New Post </a>
+                    <a href="#" class="flex items-center justify-center w-full px-4 py-2 text-base font-medium text-white border border-transparent rounded-md shadow-sm bg-primary hover:bg-primary-dark"> New Post </a>
 
                     <div class="flex justify-center mt-6">
                         <a href="#" class="text-base font-medium text-gray-900 hover:underline"> Go Premium </a>
@@ -263,7 +211,7 @@
                     <div class="px-4 sm:px-0">
                         <div class="sm:hidden">
                             <label for="question-tabs" class="sr-only">Select a tab</label>
-                            <select id="question-tabs" class="block w-full text-base font-medium text-gray-900 border-gray-300 rounded-md shadow-sm focus:border-rose-500 focus:ring-rose-500">
+                            <select id="question-tabs" class="block w-full text-base font-medium text-gray-900 border-gray-300 rounded-md shadow-sm focus:border-primary focus:ring-primary">
                                 <option selected>Recent</option>
 
                                 <option>Most Liked</option>
@@ -276,7 +224,7 @@
                                 <!-- Current: "text-gray-900", Default: "text-gray-500 hover:text-gray-700" -->
                                 <a href="#" aria-current="page" class="relative flex-1 min-w-0 px-6 py-4 overflow-hidden text-sm font-medium text-center text-gray-900 bg-white rounded-l-lg group hover:bg-gray-50 focus:z-10">
                                     <span>Recent</span>
-                                    <span aria-hidden="true" class="bg-rose-500 absolute inset-x-0 bottom-0 h-0.5"></span>
+                                    <span aria-hidden="true" class="bg-primary absolute inset-x-0 bottom-0 h-0.5"></span>
                                 </a>
 
                                 <a href="#" class="relative flex-1 min-w-0 px-6 py-4 overflow-hidden text-sm font-medium text-center text-gray-500 bg-white hover:text-gray-700 group hover:bg-gray-50 focus:z-10">
@@ -442,9 +390,9 @@
                                                     </p>
                                                 </div>
                                                 <div class="flex-shrink-0">
-                                                    <button type="button" class="inline-flex items-center px-3 py-0.5 rounded-full bg-rose-50 text-sm font-medium text-rose-700 hover:bg-rose-100">
+                                                    <button type="button" class="inline-flex items-center px-3 py-0.5 rounded-full bg-blue-50 text-sm font-medium text-primary-dark hover:bg-primary-muted">
                                                         <!-- Heroicon name: solid/plus-sm -->
-                                                        <svg class="-ml-1 mr-0.5 h-5 w-5 text-rose-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                        <svg class="-ml-1 mr-0.5 h-5 w-5 text-primary-light" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                                             <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd" />
                                                         </svg>
                                                         <span> Follow </span>
