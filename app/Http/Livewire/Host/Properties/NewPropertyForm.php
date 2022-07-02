3f73e9cc63e78bd59615d1fd6231a56a4b136280
Marc Hershey <marchershey@users.noreply.github.com>
2022-07-01 20:13:24 -0400

    backup

---
+++ app/Http/Livewire/Host/Properties/NewPropertyForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Host\Properties;
+
+use Livewire\Component;
+
+class NewPropertyForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.host.properties.new-property-form');
+    }
+}
--- resources/views/pages/host/properties.blade.php
+++ resources/views/pages/host/properties.blade.php
@@ -24,7 +24,7 @@
                   From: "translate-x-0"
                   To: "translate-x-full"
               -->
-                        <div class="w-screen max-w-md pointer-events-auto">
+                        <div class="w-screen max-w-xl pointer-events-auto">
                             <form class="flex flex-col h-full bg-white divide-y divide-gray-200 shadow-xl">
                                 <div class="flex-1 h-0 overflow-y-auto">
                                     <div class="px-4 py-6 bg-primary-dark sm:px-6">
@@ -48,6 +48,25 @@
                                         <div class="px-4 divide-y divide-gray-200 sm:px-6">
                                             <div class="pt-6 pb-5 space-y-6">
                                                 <h3 class="panel-heading">Property Information</h3>
+
+
+                                                <div class="flex gap-5">
+                                                    <label for="amen1">
+                                                        <input id="amen1" type="checkbox" value="red" class="sr-only peer">
+                                                        <div class="bg-gray-100 text-muted button peer-checked:bg-red-500">
+                                                            Pool
+                                                        </div>
+                                                    </label>
+                                                    <label for="amen1">
+                                                        <input id="amen1" type="checkbox" value="red" class="sr-only peer">
+                                                        <div class="bg-gray-100 text-muted button peer-checked:bg-red-500">
+                                                            Pool
+                                                        </div>
+                                                    </label>
+                                                </div>
+
+
+
                                                 <div>
                                                     <label for="" class="input-label">Property Name</label>
                                                     <input type="text" name="" id="" class="input">
@@ -65,18 +84,57 @@
                                                         <label for="" class="input-label">State</label>
                                                         <select id="type" name="type" class="input">
                                                             <option value=""></option>
-                                                            <option>Full house</option>
-                                                            <option>Apartment</option>
-                                                            <option>Cabin</option>
-                                                            <option>Villa</option>
-                                                            <option>Townhouse</option>
-                                                            <option>Cottage</option>
-                                                            <option>Bungalow</option>
-                                                            <option>Houseboat</option>
-                                                            <option>Hut</option>
-                                                            <option>Farm</option>
-                                                            <option>Dome</option>
-                                                            <option>Chalet</option>
+                                                            <option value="AL">Alabama</option>
+                                                            <option value="AK">Alaska</option>
+                                                            <option value="AZ">Arizona</option>
+                                                            <option value="AR">Arkansas</option>
+                                                            <option value="CA">California</option>
+                                                            <option value="CO">Colorado</option>
+                                                            <option value="CT">Connecticut</option>
+                                                            <option value="DE">Delaware</option>
+                                                            <option value="DC">District Of Columbia</option>
+                                                            <option value="FL">Florida</option>
+                                                            <option value="GA">Georgia</option>
+                                                            <option value="HI">Hawaii</option>
+                                                            <option value="ID">Idaho</option>
+                                                            <option value="IL">Illinois</option>
+                                                            <option value="IN">Indiana</option>
+                                                            <option value="IA">Iowa</option>
+                                                            <option value="KS">Kansas</option>
+                                                            <option value="KY">Kentucky</option>
+                                                            <option value="LA">Louisiana</option>
+                                                            <option value="ME">Maine</option>
+                                                            <option value="MD">Maryland</option>
+                                                            <option value="MA">Massachusetts</option>
+                                                            <option value="MI">Michigan</option>
+                                                            <option value="MN">Minnesota</option>
+                                                            <option value="MS">Mississippi</option>
+                                                            <option value="MO">Missouri</option>
+                                                            <option value="MT">Montana</option>
+                                                            <option value="NE">Nebraska</option>
+                                                            <option value="NV">Nevada</option>
+                                                            <option value="NH">New Hampshire</option>
+                                                            <option value="NJ">New Jersey</option>
+                                                            <option value="NM">New Mexico</option>
+                                                            <option value="NY">New York</option>
+                                                            <option value="NC">North Carolina</option>
+                                                            <option value="ND">North Dakota</option>
+                                                            <option value="OH">Ohio</option>
+                                                            <option value="OK">Oklahoma</option>
+                                                            <option value="OR">Oregon</option>
+                                                            <option value="PA">Pennsylvania</option>
+                                                            <option value="RI">Rhode Island</option>
+                                                            <option value="SC">South Carolina</option>
+                                                            <option value="SD">South Dakota</option>
+                                                            <option value="TN">Tennessee</option>
+                                                            <option value="TX">Texas</option>
+                                                            <option value="UT">Utah</option>
+                                                            <option value="VT">Vermont</option>
+                                                            <option value="VA">Virginia</option>
+                                                            <option value="WA">Washington</option>
+                                                            <option value="WV">West Virginia</option>
+                                                            <option value="WI">Wisconsin</option>
+                                                            <option value="WY">Wyoming</option>
                                                         </select>
                                                     </div>
                                                     <div>
@@ -86,7 +144,7 @@
                                                 </div>
 
 
-                                                <h3 class="panel-heading">Property Details</h3>
+                                                <h3 class="pt-8 panel-heading">Property Details</h3>
                                                 <div class="space-y-2">
                                                     <label for="type" class="input-label">Property Type</label>
                                                     <select id="type" name="type" class="input">
@@ -228,13 +286,28 @@
                                                     </div>
                                                 </div>
                                                 <div class="space-y-2">
-                                                    <label for="" class="input-label">Amenities</label>
-                                                    <select name="" id="" class="input" multiple>
-                                                        <option value="">one</option>
-                                                        <option value="">one</option>
-                                                        <option value="">one</option>
-                                                    </select>
-                                                    <div class="text-xs label-desc text-muted">Hold <span class="font-semibold">Crtl</span> to select multiple amenities</div>
+                                                    <span for="" class="input-label">Amenities</span>
+                                                    <div class="flex gap-5">
+
+
+
+                                                        <div>
+
+
+
+                                                            <label for="amen1">
+                                                                <input id="amen1" type="checkbox" value="red" class="sr-only peer">
+                                                                <span class="peer-checked:font-bold">amen1</span>
+                                                            </label>
+                                                            <input type="checkbox" value="orange">
+                                                            <input type="checkbox" value="yellow">
+                                                        </div>
+
+
+
+
+                                                    </div>
+
                                                 </div>
 
                                                 <h3 class="panel-heading">Property Details</h3>
