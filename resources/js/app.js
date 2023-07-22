456ed2f0107eb6a74edfa1f1ce8370f60d994089
Marc Hershey <marchershey@users.noreply.github.com>
2023-07-22 16:06:28 -0400

    dep: alpinejs/ui, alpinejs/focus

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,12 @@
 import "./bootstrap";
 
 import Alpine from "alpinejs";
+import UI from "@alpinejs/ui";
+import Focus from "@alpinejs/focus";
 import ToastComponent from "../../vendor/usernotnull/tall-toasts/resources/js/tall-toasts";
 
+Alpine.plugin(UI);
+Alpine.plugin(Focus);
 Alpine.data("ToastComponent", ToastComponent);
 
 window.Alpine = Alpine;
